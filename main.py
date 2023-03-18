e67c26f9aa8082f3cc4431f71a28c27e58ed3185
root user <halal.0fogs@icloud.com>
2023-03-18 22:48:12 +0000

    Update 2 files
    - /main.py
    - /README.md

---
+++ main.py
@@ -0,0 +1,48 @@
+# A stress testing tool for MC servers, lower TPS with just a crafting table.
+import time
+import pyautogui
+from colorama import init, Fore, Back, Style
+
+init()
+
+# Start screen
+print(Fore.YELLOW + "easyLag " + Style.RESET_ALL + "- a stress testing tool for MC servers")
+print(Fore.BLUE + "---------------------------------" + Style.RESET_ALL)
+print("Mouse position will be recorded twice. Make sure your mouse is over the recipes")
+print("before pressing enter")
+input("When you're ready, press Enter to continue...\n")
+
+# Grabbing mouse position
+pos1 = pyautogui.position()
+print(pos1)
+print(Fore.YELLOW + "Next position in 5 seconds\n" + Style.RESET_ALL)
+
+time.sleep(5)
+
+pos2 = pyautogui.position()
+print(pos2)
+print(Fore.YELLOW + "Positions saved...\n" + Style.RESET_ALL)
+
+# Asks user how long to run the process
+while True:
+    value = input("How long would you like to run the script (in seconds)? ")
+    try:
+        val = int(value)
+        if val < 0:
+            print("Sorry, input must be a positive number\n")
+            continue
+        break
+    except ValueError:
+        print("That's not a number\n")     
+
+print(Fore.YELLOW + "\nProcess will start soon, make sure you have the Minecraft window selected\n" + Style.RESET_ALL)
+time.sleep(5)
+
+# Process started
+print(Fore.GREEN + "Started" + Style.RESET_ALL)
+t_end = time.time() + int(value)
+while time.time() < t_end:
+		pyautogui.keyDown('shift')
+		pyautogui.click(pos1, _pause = 0)
+		pyautogui.click(pos2, _pause = 0)
+		pyautogui.keyUp('shift')
